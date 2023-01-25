638bfa03cfacf53e089d02b6b2ad20fe1e030266
renanrzo <arenan1988@gmail.com>
2023-01-25 19:56:31 -0300

    modificações feitas

--- type-boolean.js
+++ type-boolean.js
@@ -12,3 +12,12 @@ const texto1 = "Alura";
 const texto2 = "alura";
 
 console.log(texto1 === texto2);
+
+//teste case sensitive js
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
--- type-number.js
+++ type-number.js
@@ -34,3 +34,5 @@ var a = 0
 var b = 0
 console.log(a/b) // NaN
 
+
+
