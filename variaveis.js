58b3a5ca1e4137751318a880fbe33fa7c842b092
renanrzo <arenan1988@gmail.com>
2023-01-25 21:00:52 -0300

    inclusão variaveis

---
+++ variaveis.js
@@ -0,0 +1,38 @@
+//var funciona em qualquer parte do código
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area);
+// var area;
+
+//let
+// let forma = 'retângulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retângulo') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+
+// console.log(area);
+
+//const é uma constante ou seja uma vez que colocamos ela no programa não conseguimos alterar 
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
