0a832424f74c9d99cf6dc5e59b756a66c2463310
renanrzo <arenan1988@gmail.com>
2023-01-27 22:40:46 -0300

    add new files

--- convdetipos.js
+++ convdetipos.js
@@ -2,11 +2,16 @@
 //booleanos
 
 //conversão implicita
-const numero = 456;
-const numeroString = "456";
+// const numero = 456;
+// const numeroString = "456";
 
 //Number()
 //String()
 
-console.log(numero + Number(numeroString));
+// console.log(numero + Number(numeroString));
+
+// Vamos fazer alguns exemplos de conversão de números e booleanos através de String():
+
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); // teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
 
