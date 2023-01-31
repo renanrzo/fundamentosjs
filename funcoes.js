e302510cbbb2e78e9235a511b4a64f04c39f7559
renanrzo <arenan1988@gmail.com>
2023-01-30 21:48:41 -0300

    estudos 30-01-2023

---
+++ funcoes.js
@@ -0,0 +1,22 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função 
+                    //string
+function imprimiTexto(texto) {
+    console.log(texto);
+}
+
+// 2) executa a função uma ou mais vezes
+
+imprimiTexto(soma());
+imprimiTexto("Outro texto");
+
+//tres formas de escrever funções 
+
+function soma() {
+   return 2 + 2;
+}
+
+// console.log(soma())
---
+++ template-strings.js
@@ -0,0 +1,16 @@
+// const nome = "Ju";
+// const idade = 2021 - 1981;
+// const cidadeDeNascimento = "São Paulo";
+
+// const apresentacao = `Meu nome é ${nome}, eu tenho ${idade} anos de idade e nasci na cidade de ${cidadeDeNascimento}`
+
+// console.log(apresentacao);
+
+const nome = "Leo";
+const idade = 23;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`
+console.log(pedido)
+
